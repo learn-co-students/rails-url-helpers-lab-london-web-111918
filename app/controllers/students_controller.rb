0c2c149da95760d8eb1602ad9c1766812b8eb6cf
jodyneckles <jody@technative.tv>
2018-12-17 16:27:06 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,25 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
-  
+    before_action :set_student, only: :show
+
   def index
     @students = Student.all
   end
 
-  def show
+  def show # the before_action pastes the private method in here
   end
+
+  def activate_student
+   student = Student.find(params[:id])
+   student.active == false ? (student.active = true) : (student.active = false)
+   student.save
+   redirect_to student_path
+ end
+
 
   private
 
-    def set_student
-      @student = Student.find(params[:id])
-    end
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
 end
